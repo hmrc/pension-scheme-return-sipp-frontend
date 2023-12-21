d3437f2be5b3d941b73b844db20767c0350c4e61
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2023-12-21 15:22:45 +0000

    PSR-666-Google Tag Manager test fix

--- test/controllers/auth/SignedOutControllerSpec.scala
+++ test/controllers/auth/SignedOutControllerSpec.scala
@@ -16,12 +16,11 @@
 
 package controllers.auth
 
-import base.SpecBase
+import controllers.ControllerBaseSpec
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
 import views.html.auth.SignedOutView
 
-class SignedOutControllerSpec extends SpecBase {
+class SignedOutControllerSpec extends ControllerBaseSpec  {
 
   "SignedOut Controller" - {
 
