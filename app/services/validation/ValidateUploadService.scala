1476497a04425a6fe7ad9dc86753210289777b0b
feyyazrc <95756811+feyyazrc@users.noreply.github.com>
2024-04-11 11:01:43 +0100

    PSR-1074 Cleanup after resolving conflicts

--- app/services/validation/ValidateUploadService.scala
+++ app/services/validation/ValidateUploadService.scala
@@ -31,9 +31,7 @@ import services.UploadService
 import uk.gov.hmrc.http.HeaderCarrier
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
 
 class ValidateUploadService @Inject()(
   uploadService: UploadService,
