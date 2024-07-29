9832b9e37de4af92e3d09c91dd34f7ea40195bbf
poscala <95225995+poscala@users.noreply.github.com>
2024-07-29 17:43:50 +0100

    PSR-1104 add View Change journey re-upload question page

---
+++ app/controllers/ViewChangeNewFileUploadController.scala
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import forms.UploadNewFileQuestionPageFormProvider
+import models.SchemeId.Srn
+import models.requests.DataRequest
+import models.{FormBundleNumber, Journey, Mode}
+import navigation.Navigator
+import pages.ViewChangeNewFileUploadPage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.SaveService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.DisplayMessage.{LinkMessage, Message}
+import viewmodels.models.{FormPageViewModel, ViewChangeNewFileQuestionPageViewModel}
+import views.html.ViewChangeUploadNewFileQuestionView
+
+import javax.inject.{Inject, Named}
+import scala.concurrent.{ExecutionContext, Future}
+
+class ViewChangeNewFileUploadController @Inject()(
+  override val messagesApi: MessagesApi,
+  @Named("sipp") navigator: Navigator,
+  identifyAndRequireData: IdentifyAndRequireData,
+  formProvider: UploadNewFileQuestionPageFormProvider,
+  view: ViewChangeUploadNewFileQuestionView,
+  saveService: SaveService,
+  val controllerComponents: MessagesControllerComponents
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(srn: Srn, journey: Journey, mode: Mode): Action[AnyContent] =
+    identifyAndRequireData.withFormBundle(srn).async { request =>
+      implicit val dataRequest: DataRequest[AnyContent] = request.underlying
+
+      val preparedForm =
+        dataRequest.userAnswers
+          .fillForm(ViewChangeNewFileUploadPage(srn, journey), ViewChangeNewFileUploadController.form(formProvider))
+
+      Future.successful(
+        Ok(view(preparedForm, ViewChangeNewFileUploadController.viewModel(srn, journey, request.formBundleNumber)))
+      )
+
+    }
+
+  def onSubmit(srn: Srn, journey: Journey, mode: Mode): Action[AnyContent] =
+    identifyAndRequireData.withFormBundle(srn).async { request =>
+      implicit val dataRequest: DataRequest[AnyContent] = request.underlying
+
+      NewFileUploadController
+        .form(formProvider)
+        .bindFromRequest()
+        .fold(
+          formWithErrors =>
+            Future.successful(
+              BadRequest(
+                view(
+                  formWithErrors,
+                  ViewChangeNewFileUploadController
+                    .viewModel(srn, journey, request.formBundleNumber)
+                )
+              )
+            ),
+          value =>
+            for {
+              updatedAnswers <- Future
+                .fromTry(dataRequest.userAnswers.set(ViewChangeNewFileUploadPage(srn, journey), value))
+              _ <- saveService.save(updatedAnswers)
+              redirectTo <- Future
+                .successful(
+                  Redirect(navigator.nextPage(ViewChangeNewFileUploadPage(srn, journey), mode, updatedAnswers))
+                )
+            } yield redirectTo
+        )
+    }
+}
+
+object ViewChangeNewFileUploadController {
+
+  def form(formProvider: UploadNewFileQuestionPageFormProvider): Form[Boolean] = formProvider(
+    s"viewChangeNewFileUpload.error.required"
+  )
+  def viewModel(
+    srn: Srn,
+    journey: Journey,
+    fbNumber: FormBundleNumber
+  ): FormPageViewModel[ViewChangeNewFileQuestionPageViewModel] =
+    ViewChangeNewFileQuestionPageViewModel(
+      title = Message("viewChangeNewFileUpload.interestLandProperty.title"),
+      heading = Message("viewChangeNewFileUpload.interestLandProperty.heading"),
+      question = Message("viewChangeNewFileUpload.question"),
+      hint = Message("viewChangeNewFileUpload.hint"),
+      downloadLink = LinkMessage(
+        Message("viewChangeNewFileUpload.interestLandProperty"),
+        controllers.routes.DownloadCsvController
+          .downloadEtmpFile(
+            srn,
+            journey,
+            Some(fbNumber.value),
+            None,
+            None
+          )
+          .url
+      ),
+      onSubmit = routes.ViewChangeNewFileUploadController.onSubmit(srn)
+    )
+}
---
+++ test/controllers/ViewChangeNewFileUploadControllerSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.ViewChangeNewFileUploadController.{form, viewModel}
+import forms.UploadNewFileQuestionPageFormProvider
+import models.Journey.InterestInLandOrProperty
+import models.{FormBundleNumber, Journey, UserAnswers}
+import pages.TaskListStatusPage
+import views.html.ViewChangeUploadNewFileQuestionView
+
+class ViewChangeNewFileUploadControllerSpec extends ControllerBaseSpec {
+
+  "ViewChangeNewFileUploadControllerSpec - InterestInLandOrProperty" - {
+    new TestScope(InterestInLandOrProperty)
+  }
+
+//  "ViewChangeNewFileUploadControllerSpec - ArmsLengthLandOrProperty" - {
+//    new TestScope(ArmsLengthLandOrProperty)
+//  } TODO: fix me for other journeys
+
+  class TestScope(journey: Journey) {
+
+    private lazy val onPageLoad = controllers.routes.ViewChangeNewFileUploadController.onPageLoad(srn)
+
+    private val answers: UserAnswers = defaultUserAnswers
+      .unsafeSet(
+        TaskListStatusPage(srn, journey),
+        TaskListStatusPage.Status(completedWithNo = true, 1)
+      )
+
+    act.like(renderView(onPageLoad, answers, Seq(("fbNumber", fbNumber))) { implicit app => implicit request =>
+      injected[ViewChangeUploadNewFileQuestionView]
+        .apply(
+          form(injected[UploadNewFileQuestionPageFormProvider]),
+          viewModel(srn, journey, FormBundleNumber(fbNumber))
+        )
+    })
+
+    act.like(journeyRecoveryPage(onPageLoad).updateName("onPageLoad " + _))
+
+    //act.like(saveAndContinue(onSubmit, "value" -> "true")) TODO: fix me after integrating with View Change Tasklist
+
+    //act.like(journeyRecoveryPage(onSubmit).updateName("onSubmit " + _))  TODO: fix me after integrating with View Change Tasklist
+
+  }
+}
