03c9095c327862b55f573470d8170d88f135f260
kberec <26416940+kberec@users.noreply.github.com>
2025-03-06 16:50:45 +0000

    PSR-1897 Refactoring and library upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,10 +1,9 @@
-import play.core.PlayVersion
 import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.9.0"
-  private val hmrcMongoVersion = "2.3.0"
+  private val bootstrapVersion = "9.10.0"
+  private val hmrcMongoVersion = "2.5.0"
   private val pekkoVersion = "1.1.0"
 
   private val pekko = "org.apache.pekko"
@@ -20,7 +19,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"             % "11.8.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"             % "11.11.0",
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
@@ -28,12 +27,12 @@ object AppDependencies {
     "org.typelevel"              %% "cats-core"                              % "2.12.0",
     "eu.timepit"                 %% "refined"                                % "0.11.2",
     "uk.gov.hmrc"                %% "domain-play-30"                         % "10.0.0",
-    "uk.gov.hmrc"                %% "crypto-json-play-30"                    % "8.1.0",
+    "uk.gov.hmrc"                %% "crypto-json-play-30"                    % "8.2.0",
     "io.circe"                   %% "circe-generic"                          % "0.14.9",
     "co.fs2"                     %% "fs2-reactive-streams"                   % "3.10.2",
     "com.beachape"               %% "enumeratum-play-json"                   % "1.8.1",
     "com.softwaremill.quicklens" %% "quicklens"                              % "1.9.7",
-    "uk.gov.hmrc.objectstore"    %% "object-store-client-play-30"            % "2.0.0",
+    "uk.gov.hmrc.objectstore"    %% "object-store-client-play-30"            % "2.1.0",
     "org.apache.pekko"           %% "pekko-connectors-csv"                   % "1.0.2",
   ) ++ dependencyOverrides
 
