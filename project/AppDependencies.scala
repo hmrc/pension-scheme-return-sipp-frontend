aef7938da320b694c287326abeb0db66c3431153
narendrapaduchuri <narendra.paduchuri@digital.hmrc.gov.uk>
2025-03-26 12:31:08 +0000

    PSR-1907 platops update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.10.0"
-  private val hmrcMongoVersion = "2.5.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.6.0"
   private val pekkoVersion = "1.1.0"
 
   private val pekko = "org.apache.pekko"
@@ -19,7 +19,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"             % "11.11.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"             % "11.13.0",
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
