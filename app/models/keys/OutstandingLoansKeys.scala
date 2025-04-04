bfb07a6aa9071dc159e0194a42312cb4250511fa
narendrapaduchuri <115544611+narendrapaduchuri@users.noreply.github.com>
2025-04-04 10:40:22 +0100

    PSR-1920 SIPP - TEMPLATE - Outstanding loans made to someone else (#363)

--- app/models/keys/OutstandingLoansKeys.scala
+++ app/models/keys/OutstandingLoansKeys.scala
@@ -22,7 +22,7 @@ object OutstandingLoansKeys extends CommonKeys {
   val recipientNameOfLoan = "What is the name of the recipient of loan?"
   val dateOfOutstandingLoan = "What was the date of loan?"
   val amountOfLoan = "What is the amount of the loan?"
-  val isLoanAssociatedWithConnectedParty = "Is the loan associated with a connected party?"
+  val isLoanAssociatedWithConnectedParty = "Is the loan to a connected party?"
   val repaymentDate = "What is the repayment date of the loan?"
   val interestRate = "What is the Interest Rate for the loan?"
   val hasSecurity = "Has security been given for the loan?"
@@ -30,7 +30,7 @@ object OutstandingLoansKeys extends CommonKeys {
     "In respect of this loan, what is the total amount of Capital repayments and interest payments have been received by the scheme during the year?"
   val anyArrears = "In respect of this loan, are there any arrears outstanding from previous years?"
   val arrearsOutstandingPrYearsAmt = "If arrears are outstanding from previous years, enter the amount"
-  val outstandingAmount = "In respect of this loan, what is the amount outstanding at the year end?"
+  val outstandingAmount = "In respect of this loan, what is the total amount outstanding at the year end?"
 
   val headers: NonEmptyList[String] = NonEmptyList.of(
     firstName,
@@ -52,23 +52,23 @@ object OutstandingLoansKeys extends CommonKeys {
   )
 
   val helpers: NonEmptyList[String] = NonEmptyList.of(
-    "Question help information. The information in this row will give you hints or tips to help you to complete the required questions in the cells above.",
-    "Enter the first name of the scheme member. Mandatory question",
-    "Enter the last name of the scheme member. Mandatory question",
-    "Use the format DD-MM-YYYY. Mandatory question.",
-    "Enter the individuals National Insurance Number. If you do not know this, add the reason why you do not have this in the next column. 9 characters in alphanumeric in the format for example: AA999999A. Mandatory question.",
-    "Enter reason for not having the members National Insurance number. Maximum of 160 characters.",
-    "Enter name. Maximum 160 characters. Mandatory question",
-    "Use the format DD-MM-YYYY. Mandatory question",
-    "Enter the total amount in GBP (pounds and pence). Enter amount as whole number and to two decimal places. Mandatory question.",
-    "Enter YES or NO. Mandatory question.",
-    "Use format DD-MM-YYYY. Mandatory question.",
-    "Enter the % Enter percentage as whole number and to two decimal places. Mandatory question.",
-    "Enter YES or NO. Mandatory question.",
-    "Enter the amount in GBP (pounds and pence) Enter amount as whole number and to two decimal places",
-    "Enter YES or NO. Mandatory question.",
-    "Mandatory if there are arrears outstanding from previous years. Enter Enter the amount in GBP (pounds and pence). Enter amount as whole number and to two decimal places.",
-    "Enter the amount in GBP (pounds and pence). Enter amount as whole number and to two decimal places. Mandatory question.",
+    "Question help information. The information in this row will give you hints or tips to help you to complete the required questions in the cells above. What you need to do Enter your data from row 3 onwards. Do not remove any columns. Complete the questions per member marked horizontally across the columns. For all mandatory questions that don’t apply, please leave blank. For members that have multiple loans, complete one row per loan and repeat the members first name, last name and date of birth for the required number of rows",
+    "Enter the first name of the scheme member. Hyphens are accepted. Should be letters A to Z. Names have a maximum of 35 characters. Mandatory question.",
+    "Enter the last name of the scheme member. Hyphens are accepted. Should be letters A to Z. Names have a maximum of 35 characters. Mandatory question.",
+    "Use the format DD/MM/YYYY. Mandatory question.",
+    "Enter the individual's National Insurance Number. If you do not know this, add the reason why you do not have this in the next column. 9 characters in alphanumeric in the format for example: AA999999A. Mandatory question.",
+    "Enter reason for not having the member's National Insurance number. Maximum 160 characters.",
+    "Enter the name. Maximum 160 characters. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Use the format DD/MM/YYYY. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Enter the total amount in GBP (pounds and pence). Enter the amount as a whole number and to two decimal places Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Enter YES or NO. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Use format DD/MM/YYYY. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Enter the % Enter the percentage as a whole number and to two decimal places If the loan includes a variable rate, provide the current interest rate instead Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Enter YES or NO. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Enter the amount in GBP (pounds and pence) Enter the amount as a whole number and to two decimal places. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Enter YES or NO. Mandatory question if any separate loans were made or outstanding during the tax year.",
+    "Mandatory question if there are arrears outstanding from previous years. Enter the amount in GBP (pounds and pence). Enter the amount as a whole number and to two decimal places.",
+    "Enter the amount in GBP (pounds and pence). Enter the amount as a whole number and to two decimal places Mandatory question if any separate loans were made or outstanding during the tax year.",
     "ERRORS WITH DETAIL"
   )
 }
