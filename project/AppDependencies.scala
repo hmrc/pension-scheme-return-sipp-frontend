687c399123447d38ad767151eabbbc39b712f458
MadStu <737342+MadStu@users.noreply.github.com>
2026-03-06 10:37:52 +0000

    [PSR-2178] Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
   private val pekkoVersion = "1.4.0"
 
@@ -19,8 +19,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"             % "12.31.0",
-    "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"  % "3.4.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"             % "12.32.0",
+    "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"  % "3.5.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"                %% "tax-year"                               % "6.0.0",
@@ -29,7 +29,7 @@ object AppDependencies {
     "uk.gov.hmrc"                %% "domain-play-30"                         % "13.0.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"                    % "8.4.0",
     "co.fs2"                     %% "fs2-reactive-streams"                   % "3.12.2",
-    "com.beachape"               %% "enumeratum-play-json"                   % "1.9.4",
+    "com.beachape"               %% "enumeratum-play-json"                   % "1.9.5",
     "com.softwaremill.quicklens" %% "quicklens"                              % "1.9.12",
     "uk.gov.hmrc.objectstore"    %% "object-store-client-play-30"            % "2.5.0",
     "org.apache.pekko"           %% "pekko-connectors-csv"                   % "1.2.0",
@@ -40,7 +40,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"          %% "scalacheck-1-18"         % "3.2.19.0",
     "org.scalatestplus.play"     %% "scalatestplus-play"      % "7.0.2",
-    "org.jsoup"                  %  "jsoup"                   % "1.21.2"
+    "org.jsoup"                  %  "jsoup"                   % "1.22.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
