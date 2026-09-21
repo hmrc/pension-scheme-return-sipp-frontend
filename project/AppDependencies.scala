fc68da50bf58ec0b88dc69f9b9c4cf6718f68ab2
andrewbrown22 <45789050+andrewbrown22@users.noreply.github.com>
2026-09-21 13:35:34 +0100

    DDCNL-12392 PR comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
   private val pekkoVersion = "1.7.0"
 
   private val dependencyOverrides = Seq(
